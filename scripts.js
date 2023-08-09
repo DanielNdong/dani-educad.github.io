9c5827db6d92e498a4570a2010aa7cb5826f551b
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-08-09 17:43:53 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+/* Mostrar y cerrar el menu del header */
+document.querySelector("#menu-button").addEventListener("click", () => {
+    document.querySelector(".menu__header").classList.toggle("show");
+})
