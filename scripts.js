267590ad5d35a658db94f961eedf0f42b507530e
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-08-28 16:31:26 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,3 +2,18 @@
 document.querySelector("#menu-button").addEventListener("click", () => {
     document.querySelector(".menu__header").classList.toggle("show");
 })
+
+
+document.querySelectorAll(".faq__header").forEach(item => {
+        item.addEventListener("click", () => {
+            if(item.classList.contains("active")) return item.classList.remove("active")
+            document.querySelectorAll(".faq__header").forEach(item => {
+                if(item.classList.contains("active")) {
+                    item.classList.remove("active")
+                }
+            })
+            item.classList.add("active")
+        })
+
+    }) 
+   
