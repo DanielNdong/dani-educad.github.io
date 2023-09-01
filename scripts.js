60cc94569da53d9f4ee7335ed40edda581fed2f3
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-09-01 17:14:58 +0100

    color and script of hamburger

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 /* Mostrar y cerrar el menu del header */
 document.querySelector("#menu-button").addEventListener("click", () => {
     document.querySelector(".menu__header").classList.toggle("show");
+    document.querySelector("#menu-button").classList.toggle("is-active");
 })
 
 
+/* Accordeon script to open o close faq's */
 document.querySelectorAll(".faq__header").forEach(item => {
         item.addEventListener("click", () => {
             if(item.classList.contains("active")) return item.classList.remove("active")
