29af36cb4c71a43257e9cde98233959086200cfc
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-09-02 21:36:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ class HolaMundo extend HTMLElement {
     this.inneHTML = "<h1>HOLA MUNDO</h1>"
   }
 
-  window.customElement.define("hola-mundo", HolaMundo)
 
 }
+ customElement.define("hola-mundo", HolaMundo)
+
+class ElementoEspecifico extend HTMLElement {
+
+  constructor() {
+  super()
+
+    this.innerHTML = "<p>Este es un componento con un elemento especifico<p>"
+}
+
+}
+
+customElement.defin("elemento-especifico", ElementoEspecifico, options: {extends: 'button'})
