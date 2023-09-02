c5697de14c119fc20d3c6ca67672b03facc8f9e6
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-09-02 21:31:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+class HolaMundo extend HTMLElement {
 
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    const gif = this.getAttribute("gif")
+    this.inneHTML = "<h1>HOLA MUNDO</h1>"
+  }
+
+  window.customElement.define("hola-mundo", HolaMundo)
+
+}
