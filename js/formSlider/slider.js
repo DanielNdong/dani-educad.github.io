ddbabc130acef734991132813a3d7cd71e7ae595
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-10-03 16:06:02 +0200

    Update slider.js

--- js/formSlider/slider.js
+++ js/formSlider/slider.js
@@ -15,22 +15,20 @@
       const currentElement = document.querySelector('.esta') 
           /* document.querySelector('#next').remove(); */
         const sumbitButton = document.createElement('button');
-        console.log(sumbitButton);
         sumbitButton.classList.add('boton_enviar');
         sumbitButton.innerText = 'Regístrate';
         currentElement.appendChild(sumbitButton);
     }
 
+    /* Cambiamos el paso del registro en el que nos encontramos*/
      function changeView(change) {
         const currentElement = Number(document.querySelector('.register_section--show').dataset.id)
         
         value = currentElement;
         value += change;
-        console.log(REGISTER_SECTION)
         if(value === 0  || value === REGISTER_SECTION.length+1) {
             value = value === 0 ? 1 : REGISTER_SECTION.length;
         }
-        console.log(currentElement-1)
         REGISTER_SECTION[currentElement - 1].classList.toggle('register_section--show')
         REGISTER_SECTION[value - 1].classList.toggle('register_section--show')
     }
