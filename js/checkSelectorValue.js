426a91be578a6d8993f1056d16de55d20e44560e
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-10-04 18:23:28 +0200

    Update checkSelectorValue.js

--- js/checkSelectorValue.js
+++ js/checkSelectorValue.js
@@ -1,1 +1,10 @@
-const selectValue = document.querySelector("#selectNivelAcademico").value 
+const selectorElement = document.querySelector(".nivelAcademico");
+const ramaBach = document.querySelector(".ramaBach");
+
+selectorElement.addEventListener("change", ()=> {
+  const selectValue = document.querySelector(".nivelAcademico").value;
+  console.log("Cambio");
+  if(selectValue !== "bachillerato") {
+      ramaBach.style.display = "initial";
+  }
+})
