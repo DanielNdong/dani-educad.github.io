388a65880be76270afacc5810ef22690bcc19116
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-10-05 16:43:34 +0200

    Update checkSelectorValue.js

--- js/checkSelectorValue.js
+++ js/checkSelectorValue.js
@@ -1,7 +1,14 @@
 const selectorElement = document.querySelector(".nivelAcademico");
-const ramaBach = document.querySelector(".ramaBach");
+const selectorElement = document.querySelector(".nivelAcademico");
+const selectorElement = document.querySelector(".nivelAcademico");
+const selectorElement = document.querySelector(".nivelAcademico");
+const selectorElement = document.querySelector(".nivelAcademico");
 
+const ramaBach = document.querySelector(".ramaBach");
+/* Ocultamos o mostramos el select en caso de que la rama elegida sea 'bachillerato' */
 selectorElement.addEventListener("change", ()=> {
   const selectValue = document.querySelector(".nivelAcademico").value;
       ramaBach.style.display = selectValue === "bachillerato" ? 'initial' : 'none';
 })
+
+/* Añadir mas materias que puede impartir el profesor */
