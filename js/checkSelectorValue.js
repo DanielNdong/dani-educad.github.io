df93779c290098c3a8ea50f7c38488d629c79e26
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-10-13 12:24:31 +0200

    Update checkSelectorValue.js

--- js/checkSelectorValue.js
+++ js/checkSelectorValue.js
@@ -1,18 +1,23 @@
-const selectElement = [...document.querySelectorAll(".nivelAcademico")];
+const selectElement = document.querySelectorAll(".nivelAcademico");
+const ramaBach = [...document.querySelectorAll(".ramaBach")];
 
 const selectLabel = document.querySelector(".labelNivelAcademico");
 const cursoLabel = document.querySelector(".cursoLabel");
 const materiaLabel = document.querySelector(".materiaLabel");
-const ramaBach = [...document.querySelectorAll(".ramaBach")];
 
 /* Ocultamos o mostramos el select en caso de que la rama elegida sea 'bachillerato' */
-selectElement.forEach((item, index)=>{
-  item.addEventListener("change", ()=> {
-  const selectValue = item.value;
-      ramaBach[index].style.display = selectValue === "bachillerato" ? 'initial' : 'none';
-  })
+[...selectElement].forEach((item, index)=>{
+  console.log("elemento: "+item.value+" con indice: "+index)
+  item.addEventListener("change", changeDisplayRama(index))
 })
 
+/* Cambiar la visualizacion del campo de rama de bachillerato */
+const changeDisplayRama = (index)=>{
+      selectValue = this.value;
+      [...document.querySelectorAll(".ramaBach")][index].style.display = selectValue === "bachillerato" ? 'initial' : 'none';
+}
+
+                           
 const addNewSubjectButton = document.querySelector(".appendSubject");
 addNewSubjectButton.addEventListener("click", ()=> {
  appendNewsElements();
