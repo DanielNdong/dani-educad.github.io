09f9259cca7130ce528f79874b310d83cfbd98e3
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-11-07 16:34:45 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -101,8 +101,8 @@ header.addEventListener('scroll', () => {
 /* Abrir y cerrar el modal */
 let user = document.querySelector('.header__userIcon');
 let modal = document.querySelector('.modalStarSession');
-let gotoEducadButton = [...document.querySelector('button-link')];
-let buttonHeaderLogin = document.querySelectorAll('.modalStarSession--section > button');
+let gotoEducadButton = [...document.querySelectorAll('button-link')];
+let buttonHeaderLogin = document.querySelector('.modalStarSession--section > button');
 user.addEventListener('click', ()=> {
   if(menuMobil.className.includes('show')) menuMobil.classList.remove('show')
   modal.classList.add('active')
