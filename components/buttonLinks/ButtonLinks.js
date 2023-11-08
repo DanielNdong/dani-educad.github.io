c0c1367dd96877d0bcecb8f3311a0351fa599940
Daniel Aquilino Ndong <prowebmaking@outlook.es>
2023-11-08 07:56:17 +0100

    Update ButtonLinks.js

--- components/buttonLinks/ButtonLinks.js
+++ components/buttonLinks/ButtonLinks.js
@@ -67,7 +67,7 @@ template.innerHTML = `
         display: none;
     }
 </style>
-    <a class="link" href="${url}">
+    <a class="link" ${urls = url !== '' ? `href=${url}` : '' }>
         <slot name="text">Texto editable</slot>
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-circle-arrow-right" width="32" height="32" viewBox="0 0 24 24" stroke-width="1" stroke="#0f0d3e" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
